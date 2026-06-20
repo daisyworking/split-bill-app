f7acb3dd64ee4597a100ac0a31bb5c8cb17a0b1b
daisyworking <daisyomi310@gmail.com>
2026-06-20 19:15:21 +0800

    首次打包上傳

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBH4ez8u4skkyTsUTXPtBMHT0afVlXvsMM",
+  authDomain: "rats-split-bill.firebaseapp.com",
+  projectId: "rats-split-bill",
+  storageBucket: "rats-split-bill.firebasestorage.app",
+  messagingSenderId: "252272732529",
+  appId: "1:252272732529:web:8065d868ca19e0b2732ed0",
+  measurementId: "G-7BDYQYSC8P"
+}
